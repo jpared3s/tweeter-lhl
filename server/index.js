7fc04033e8ef67f5cec951828169828572490aa5
jpared3s <jparedes.wall@gmail.com>
2023-01-30 16:59:23 -0500

    tweeter assignment

--- server/index.js
+++ server/index.js
@@ -14,8 +14,7 @@ app.use(express.static("public"));
 const db = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
+// This simple interface layer has a big benefit: we could switch out the actual database it uses and see little to no changes elsewhere in the code
 // (hint hint).
 //
 // Because it exports a function that expects the `db` as a parameter, we can

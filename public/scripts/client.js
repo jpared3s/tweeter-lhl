fb7a4eb24e280d8b09931e210a8f9bdee8d88a99
jpared3s <jparedes.wall@gmail.com>
2023-02-02 09:58:05 -0500

    cross scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,44 +7,48 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 
 $(document).ready(function() {
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  console.log(div.innerHTML)
+  return div.innerHTML;
+};
 
 const renderTweets = function(tweets) {
   for(tweetObj of tweets) {
-    // console.log("===================", tweetObj)
     const $tweetElement = createTweetElement(tweetObj);
     $('#tweets-container').append($tweetElement)
   }
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
 }
 
 // took in article from index.html and using template literal inserted values
 const createTweetElement = function(tweet) {
+  $("time.timeago").timeago();
   let $tweet = `
   <article class="tweet-container">
   <header>
@@ -56,9 +60,9 @@ const createTweetElement = function(tweet) {
       ${tweet.user.handle}
     </div>
   </header>
-  <p class="user-post">${tweet.content.text}</p>
+  <p class="user-post">${escape(tweet.content.text)}</p>
   <footer> 
-    ${tweet.created_at}
+    ${$.timeago(tweet.created_at)}
     <span>
       <i class="fa-sharp fa-solid fa-flag"></i>
       <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -66,24 +70,34 @@ const createTweetElement = function(tweet) {
     </span>
   </footer>
 </article>`
-// console.log('++++++++++++++', $tweet)
   return $tweet;
 }
 
 $('.tweet-form').submit(function (evt) {
   evt.preventDefault();
   const tweetData = $('#tweet-text').val()
-  //validate data here
+  if (!tweetData.length) {
+    alert("No text detected.")
+  } else if (tweetData.length > 140) {
+    alert("Your post must be less then 140 characters")
+  }
   const serializeTweet = $(this).serialize()
-  console.log("#1 ", tweetData)
-  console.log("#2 ", serializeTweet)
-  console.log("#0 ", this)
+  $.post('http://localhost:8080/tweets', serializeTweet,(result) => {
+    loadTweets()
+  })
 })
 
+const loadTweets = () => {
+  $.get('http://localhost:8080/tweets', (data) => { 
+    console.log("Fetched succesfully: ", data)
+    renderTweets(data);
+  }) 
+}
+
+loadTweets();
 
 // createTweetElement(tweetData[1]);
-renderTweets(tweetData);
-
+// renderTweets(tweetData);
 // Test / driver code (temporary)
 // console.log($tweet); // to see what it looks like
 // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
